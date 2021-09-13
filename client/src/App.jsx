7b489970824f59155cec1031e73dfefa2f4ce49a
Toi Vũ <58621888+toivux8@users.noreply.github.com>
2021-09-13 21:07:42 +0700

    Connect client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import {StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
-import Cookie from 'universal-cookie';
+import Cookies from 'universal-cookie';
 import { ChannelListContainer, ChannelContainer, Auth } from './components';
 import './App.css';
 
+const cookies = new Cookies();
+
 const apiKey = 'h5b9f7fey66c';
+const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey);
 
-const authToken = false;
+if(authToken) {
+    client.connectUser({
+        name: cookies.get( 'userName'),
+        fullName: cookies.get( 'fullName'),
+        id: cookies.get( 'userId'),
+        phoneNumber: cookies.get( 'phoneNumber'),
+        image: cookies.get( 'avatarURL'),
+        hashedPassword: cookies.get( 'hashedPassword')
+    }, authToken );
+}
 
 const App = () => {
     if(!authToken) return <Auth />
