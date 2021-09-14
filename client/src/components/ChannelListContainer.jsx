4378ab5256638c633bf90ba05a96414cf767b7af
Toi Vũ <58621888+toivux8@users.noreply.github.com>
2021-09-14 08:45:43 +0700

    login and logout

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -5,7 +5,9 @@ import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
 import HospitalIcon from '../assets/hospital.png'
 import LogoutIcon from '../assets/logout.png'
 
-const SideBar = () => (
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => (
     <div className="channel-list__sidebar">
         <div className="channel-list__sidebar__icon1">
             <div className="icon1__inner">
@@ -13,7 +15,7 @@ const SideBar = () => (
             </div>
         </div>
         <div className="channel-list__sidebar__icon2">
-            <div className="icon1__inner">
+            <div className="icon1__inner" onClick = { logout }>
                 <img src={LogoutIcon} alt="Logout" width="30" />
             </div>
         </div>
@@ -27,9 +29,20 @@ const CompanyHeader = () => (
 )
 
 const ChannelListContainer = () => {
+    const logout = () => {
+        cookies.remove("token");
+        cookies.remove( 'userName');
+        cookies.remove( 'fullName');
+        cookies.remove( 'userId');
+        cookies.remove( 'phoneNumber');
+        cookies.remove( 'avatarURL');
+        cookies.remove( 'hashedPassword');
+
+        window.location.reload();
+    }
     return (
         <>
-            <SideBar />
+            <SideBar logout = { logout } />
             <div className="channel-list__list__wrapper">
                 <CompanyHeader />
                 <ChannelSearch />
